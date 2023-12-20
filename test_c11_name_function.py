252b31cc666850b9c1ae39217975624b6a3c141a
aiXpert <45121432+aiXpertLab@users.noreply.github.com>
2023-12-20 11:52:27 -0500

    bf c18 django

--- test_c11_name_function.py
+++
@@ -1,5 +0,0 @@
-from c11_name_function import get_formatted_name
-def test_first_last_name():
-    """Do names like 'Janis Joplin' work?"""
-    formatted_name = get_formatted_name('janis', 'joplin')
-    assert formatted_name == 'Janis Jo1plin'
