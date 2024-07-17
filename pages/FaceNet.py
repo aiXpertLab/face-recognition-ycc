b2c17dc9a01f08a6cb805c3baa177c8cdb620767
aixprtlab <aixpertlab@gmail.com>
2024-07-17 11:55:38 -0400

    v4.new model facenet_complete_model_leo.h5

--- pages/FaceNet.py
+++ pages/FaceNet.py
@@ -2,19 +2,18 @@ import streamlit as st
 from utils import streamlit_components
 streamlit_components.streamlit_ui('🦣 Face Detection With MTCNN')
 # -------------------------------------------------------------------------------------
-#Step 1
-from utils import inception_resnet_v1
+# #Step 1
+# from utils import inception_resnet_v1
 
-#Step 2
-model = inception_resnet_v1.InceptionResNetV1()
-model.load_weights('./models/facenet_keras_weights.h5')
+# #Step 2
+# model = inception_resnet_v1.InceptionResNetV1()
+# model.load_weights('./models/facenet_keras_weights.h5')
 
+# model.save('./models/facenet_complete_model_leo.h5')
 
+import tensorflow as tf
 
-# import tensorflow as tf
-# from keras_facenet import FaceNet
-
-# # model = tf.keras.models.load_model('./models/n/facenet_keras.h5')
+model = tf.keras.models.load_model('./models/facenet_complete_model_leo.h5')
 # # model = tf.keras.models.load_model('./models/resnet51q_imagenet.h5')
 # model = tf.keras.models.load_model('./models/GN_W1.3_S2_ArcFace_epoch48.h5')
 
